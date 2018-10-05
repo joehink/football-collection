b5eb67e8336746fa53c390d12504adc74472d7ee
Joe Hinkley <joehhinkley@gmail.com>
2018-10-05 16:24:25 -0700

    Updates

---
+++ precache-manifest.62fe32935da4a4b4b137bba8447bb2cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "eada5549f1a43d9ff1b2",
+    "url": "/football-collection/static/js/runtime~main.eada5549.js"
+  },
+  {
+    "revision": "f14cd02373e40879064f",
+    "url": "/football-collection/static/js/main.f14cd023.chunk.js"
+  },
+  {
+    "revision": "6b19b3b57994f84177a9",
+    "url": "/football-collection/static/js/1.6b19b3b5.chunk.js"
+  },
+  {
+    "revision": "43b2b98ccd76b892221ce34805d7ee12",
+    "url": "/football-collection/index.html"
+  }
+];
